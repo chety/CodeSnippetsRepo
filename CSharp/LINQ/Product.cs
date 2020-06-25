5a69b6e411cdc6e409361832dd2d8e5740cf8faf
Chety <cetincelik20@gmail.com>
2020-06-25 22:22:13 +0300

    Create Product.cs

---
+++ CSharp/LINQ/Product.cs
@@ -0,0 +1,14 @@
+namespace Dummy
+{
+  class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        
+        public override string ToString()
+        {
+            return $"Id: {Id},Name: {Name}, Price: {Price}";
+        }
+    }
+}
